a2930a84eb06ebb094caf6318dd463c04eb7e2aa
CooperKe <keice5@hotmail.com>
2015-04-04 23:31:18 +0800

    Delete the dead code at No.8045d75

--- log2grasp.py
+++ log2grasp.py
@@ -41,8 +41,6 @@ for line in lines :
 	elif inst == 'switch' :
 		out_task, in_task, tick, tick_reload, out_minitick, in_minitick = args.split(' ')
 		
-		#ut_time = (int(tick) + (int(tick_reload) - int(out_minitick)) / int(tick_reload)) / 100 * 1000;
-		#n_time  = (int(tick) + (int(tick_reload) - int(in_minitick))  / int(tick_reload)) / 100 * 1000;
 		out_time = (float(tick) + (float(tick_reload) - float(out_minitick)) / float(tick_reload)) / 100 * 1000;
 		in_time  = (float(tick) + (float(tick_reload) - float(in_minitick))  / float(tick_reload)) / 100 * 1000;
 
